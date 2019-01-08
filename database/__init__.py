25f8ff5de8b30105c55ecb524d60f0e7535b24e1
kencin <myzincx@gmail.com>
2019-01-08 16:01:51 +0800

    添加按航班号查询的机票监听功能

--- database/__init__.py
+++
@@ -1,8 +0,0 @@
-#!/usr/bin/python
-# -*- coding: UTF-8 -*-  
-# TgBot - __init__.py
-# 2019/1/7 17:18
-# Author:Kencin <myzincx@gmail.com>
-
-from database import config
-from database import toDatabase
